7d0cfc0121fc6dcf4c6193e2ff2c90bbdbdfb4d5
thisisvana <gezim@Gezims-iMac.local>
2017-02-16 20:20:51 -0800

    From home
    Commits from how thursday16feb

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php include "partials/header.php"; ?>
+
+
+<div class="main-wrapper">
+    <div class="row">
+
+
+
+    </div>
+</div>
+
+<?php include "partials/footer.php"; ?>
--- js/main.js
+++ js/main.js
@@ -55,3 +55,26 @@ function moveSlide(n) {
     dots[slideIndex-1].className += " dots-active";
     time = setTimeout(showSlides, 4000);
 }
+
+
+
+
+(function($){
+	function floatLabel(inputType){
+		$(inputType).each(function(){
+			var $this = $(this);
+			// on focus add cladd active to label
+			$this.focus(function(){
+				$this.next().addClass("active");
+			});
+			//on blur check field and remove class if needed
+			$this.blur(function(){
+				if($this.val() === '' || $this.val() === 'blank'){
+					$this.next().removeClass();
+				}
+			});
+		});
+	}
+	// just add a class of "floatLabel to the input field!"
+	floatLabel(".floatLabel");
+})(jQuery);
