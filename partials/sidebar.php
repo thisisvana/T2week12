5fb63c2e262b6e830720d4061a0736a4dc37c9ba
thisisvana <thisisvana@gmail.com>
2017-03-27 19:56:24 -0700

    monday evening

---
+++ partials/sidebar.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>VANARTS STUDENT MOCKUP</title>
+        <meta name="description" content="This is a student exercise website for the Vancouver Institute of Media Arts (www.vanarts.com)">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
+        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Alex+Brush|Arapey|Oranienbaum" rel="stylesheet">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="https://use.fontawesome.com/1845926a10.js"></script>
+    </head>
+
+    <body class="bg">
+      <header class="page-header">
+
+        <div class="sidebar">
+            <div class="row expanded">
+              <div class="side-b small-12 medium-4 large-3 columns">
+                  <div class="logo">
+                    <a href="index.php"><img src="documentation/logo/logo.svg" alt="logo"></a>
+                  </div>
+                  <div class="side-nav">
+
+                    <ul class="main-menu">
+                      <li><a href="ndp.php">ndp</a></li>
+                      <li><a href="green.php">green</a></li>
+                      <li><a href="liberal.php">liberal</a></li>
+                      <li><a href="conservative.php">conservative</a></li>
+                      <li><a href="mp.php">my mp</a></li>
+                      <li><a href="ndp.php">vote smart</a></li>
+                    </ul>
+
+                    <form method="post" action="form_submit.html">
+
+                       <label for="form-name">Subscribe</label>
+                       <input type="text" class="c-placeholder" id="form-name" name="name" placeholder="name" required />
+
+                       <!-- <label for="form-email">Email*</label><br> -->
+                       <input type="text" class="c-placeholder" id="form-email" name="email" placeholder="example@example.com" required />
+                       <input id="send-btn" type="submit" name="f_submit" class="btn" value="Send">
+                    </form>
+                    <div class="social-nav">
+                      <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                      <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                    </div>
+
+                </div>
+            </div>
