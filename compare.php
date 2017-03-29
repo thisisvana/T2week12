db13a190ffc2b2f640932f2692c33acc73d191a1
Kenyu Sakuraba <kenyu.sakuraba.1023@gmail.com>
2017-03-28 20:12:09 -0700

    added compare.php and sass

---
+++ compare.php
@@ -0,0 +1,136 @@
+<?php include "partials/sidebar.php"; ?>
+<div class="small-12 medium-8 large-9 columns">
+    <div class="compare-header">
+        <div class="small-12 medium-12 large-7 columns">
+            <div class="compare-top-left">
+                <ul class="party-list">
+                    <li>NDP</li>
+                    <li>Green</li>
+                    <li>Liberal</li>
+                    <li>Conservative</li>
+                </ul>
+                <div class="small-12 medium-12 large-9-push columns">
+                <table class="party-topic-list">
+                    <tr>
+                        <th colspan="9">
+                            Party Topic list
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            Syrian Refugees
+                        </td>
+                        <td>
+                            Marijuana
+                        </td>
+                        <td>
+                            Education
+                        </td>
+                        <td>
+                            Environment
+                        </td>
+                        <td>
+                            Health Care
+                        </td>
+                        <td>
+                            Youth Issues
+                        </td>
+                        <td>
+                            Forign policy
+                        </td>
+                        <td>
+                            Gender LGBTQA
+                        </td>
+                        <td>
+                            Economy
+                        </td>
+                    </tr>
+                </table>
+                </div>
+            </div>
+        </div>
+        <div class="small-12 medium-12 large-2 columns">
+            <div class="top-two-btn">
+            <h3 class="btn">Result</h3>
+            <h3 class="btn">Reflesh</h3>
+            </div>
+        </div>
+    </div>
+    <div class="large-12 medium-9 small-12 columns">
+        <div class="all-party-policy">
+    <div class="small-12 medium-12 large-3 columns">
+        <h3>NDP</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            \consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+<!--    <div class="small-12 medium-12 large-2 columns">
+        <h3>test</h3>
+        <h3>test</h3>
+    </div>-->
+    
+     <div class="small-12 medium-12 large-3 columns">
+        <h3>Green</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            \consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+<!--    <div class="small-12 medium-12 large-2 columns">
+        <h3>test</h3>
+        <h3>test</h3>
+    </div>-->
+    
+     <div class="small-12 medium-12 large-3 columns">
+        <h3>Liberal</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            \consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+<!--    <div class="small-12 medium-12 large-2 columns">
+        <h3>test</h3>
+        <h3>test</h3>
+    </div>-->
+    
+     <div class="small-12 medium-12 large-3 columns">
+        <h3>Conservative</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor 
+            incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            \consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+<!--    <div class="small-12 medium-12 large-2 columns">
+        <h3>test</h3>
+        <h3>test</h3>
+    </div>-->
+        </div>
+</div>
+</div>
+
+<?php include "partials/footer.php"; ?>
