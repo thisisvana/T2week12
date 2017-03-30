a13420588b55887959923299c3146a75d8ca092c
thisisvana <thisisvana@gmail.com>
2017-03-30 16:05:30 -0700

    thursday afternoon bis

---
+++ compare.php
@@ -0,0 +1,148 @@
+<?php include "partials/sidebar.php"; ?>
+<div class="page-banner small-12 medium-8 large-9 columns">
+    <div class="compare-header">
+       <div class="small-12 medium-12 large-9 columns">
+        <div class="compare-top-left">
+            <ul class="party-list">
+                <li>NDP</li>
+                <li>Green</li>
+                <li>Liberal</li>
+                <li>Conservative</li>
+            </ul>
+        </div>
+        </div>
+            <div class="small-12 medium-12 large-3 columns">
+            <div class="top-two-btn">
+                <h3 class="two-btn btn">Result</h3>
+                <h3 class="two-btn btn">Reflesh</h3>
+            </div>
+        </div>
+        <div class="small-12 medium-12 large-9 columns">
+                <div class="party-topic-list">
+
+                    <div class="small-12 medium-12 large-12 columns list-title">
+                        <h3>Parties policies</h3>
+                    </div>
+
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Syrian Refugees
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Marijuana
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Education
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Environment
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Health Care
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Youth Issues
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Forign policy
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Gender LGBTQA
+                    </div>
+                    <div class="small-12 medium-6 large-4 columns btn list">
+                        Economy
+                    </div>
+                </div>
+            </div>
+        <div class="small-12 medium-3 large-3 columns">
+        </div>
+        <!--</div>-->
+
+
+
+
+    <!--<div class="large-9 medium-8 small-12 columns">-->
+        <div class="all-party-policy small-12 medium-12 large-12 columns">
+            <div class="small-12 medium-12 large-3 columns">
+                <h2>NDP</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo
+                    \consequat. Duis aute irure dolor in reprehenderit
+                    in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <h3 class="voteBtn btn">Vote</h3>
+            </div>
+            <!--    <div class="small-12 medium-12 large-2 columns">
+                    <h3>test</h3>
+                    <h3>test</h3>
+                </div>-->
+
+            <div class="small-12 medium-12 large-3 columns">
+                <h2>Green</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo
+                    \consequat. Duis aute irure dolor in reprehenderit
+                    in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <h3 class="voteBtn btn">Vote</h3>
+            </div>
+            <!--    <div class="small-12 medium-12 large-2 columns">
+                    <h3>test</h3>
+                    <h3>test</h3>
+                </div>-->
+
+            <div class="small-12 medium-12 large-3 columns">
+                <h2>Liberal</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo
+                    \consequat. Duis aute irure dolor in reprehenderit
+                    in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <h3 class="voteBtn btn">Vote</h3>
+            </div>
+            <!--    <div class="small-12 medium-12 large-2 columns">
+                    <h3>test</h3>
+                    <h3>test</h3>
+                </div>-->
+
+            <div class="small-12 medium-12 large-3 columns">
+                <h2>Conservative</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo
+                    \consequat. Duis aute irure dolor in reprehenderit
+                    in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <h3 class="voteBtn btn">Vote</h3>
+            </div>
+            <!--    <div class="small-12 medium-12 large-2 columns">
+                    <h3>test</h3>
+                    <h3>test</h3>
+                </div>-->
+        </div>
+    <!--</div>-->
+    <!--<div class="small-12 medium-3 large-3 columns">-->
+        <!--</div>-->
+
+</div>
+</header>
+
+<?php include "partials/footer.php"; ?>
