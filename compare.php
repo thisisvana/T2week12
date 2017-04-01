db22b678580fd94214d70832e528d160b749749f
thisisvana <thisisvana@gmail.com>
2017-03-31 17:31:20 -0700

    friday morning

--- compare.php
+++ compare.php
@@ -49,18 +49,10 @@
 
 
     <!--<div class="large-9 medium-8 small-12 columns">-->
-        <div class="all-party-policy small-12 medium-12 large-12 columns">
+        <div class="all-party-policy syr-ref small-12 medium-12 large-12 columns">
             <div class="parties-pol small-12 medium-12 large-3 columns">
                 <h4>NDP</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo
-                    \consequat. Duis aute irure dolor in reprehenderit
-                    in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p>Bring in 10,000 Syrian refugees by the end of the year, and resettle 9,000 every year until 2019.
                 </p>
                 <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
             </div>
@@ -71,15 +63,7 @@
 
             <div class="parties-pol small-12 medium-12 large-3 columns">
                 <h4>Green</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo
-                    \consequat. Duis aute irure dolor in reprehenderit
-                    in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p>Eliminate the Temporary Foreign Workers Program and increase immigration where labour shortages are established.
                 </p>
                  <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
             </div>
@@ -90,50 +74,214 @@
 
             <div class="parties-pol small-12 medium-12 large-3 columns">
                 <h4>Liberal</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo
-                    \consequat. Duis aute irure dolor in reprehenderit
-                    in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p>Immediately admit 25,000 refugees from Syria, and invest an additional $100 million to increase refugee processing, sponsorship, and settlement.
                 </p>
                 <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
             </div>
-            <!--    <div class="small-12 medium-12 large-2 columns">
-                    <h3>test</h3>
-                    <h3>test</h3>
-                </div>-->
 
             <div class="parties-pol small-12 medium-12 large-3 columns">
                 <h4>Conservative</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo
-                    \consequat. Duis aute irure dolor in reprehenderit
-                    in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p>Create a new three year, $9 million program to assist persecuted religious minorities and would accept 10,000 refugees over 4 years.
                 </p>
                 <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
             </div>
-            <!--    <div class="small-12 medium-12 large-2 columns">
-                    <h3>test</h3>
-                    <h3>test</h3>
-                </div>-->
+<!-- row 2 -->
+            <div class="all-party-policy mariana small-12 medium-12 large-12 columns">
+                <div class="parties-pol small-12 medium-12 large-3 columns">
+                    <h4>NDP</h4>
+                    <p>The decriminalization of marijuana and continued public discussions.
+                    </p>
+                    <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
+                </div>
+
+                <div class="parties-pol small-12 medium-12 large-3 columns">
+                    <h4>Green</h4>
+                    <p>The complete legalization of marijuana and tax collection on the industry.
+                    </p>
+                     <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
+                </div>
+
+
+                <div class="parties-pol small-12 medium-12 large-3 columns">
+                    <h4>Liberal</h4>
+                    <p>The complete legalization of marijuana and tax collection on the industry.
+                    </p>
+                    <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
+                </div>
+
+                <div class="parties-pol small-12 medium-12 large-3 columns">
+                    <h4>Conservative</h4>
+                    <p>Complete opposition to the legalization and decriminalization of marijuana.
+                    </p>
+                    <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
+                </div>
+
+        </div>
+    <!--row 3 -->
+        <div class="all-party-policy youth small-12 medium-12 large-12 columns">
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>NDP</h4>
+                <p>$100 million to improve mental health services for youth, and an additional $200 million over 4 years to help youth find employment, including cracking down on unpaid internships.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Green</h4>
+                <p>The elimination of all tuition fees by 2020, by starting off with nixing them for low income students.
+                </p>
+                 <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
+            </div>
+
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Liberal</h4>
+                <p>$1.3 billion over 3 years to create jobs for young people, including more guides and interpreters for Park Canada.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Conservative</h4>
+                <p>Reallocate $7 million over 2 years to support the relocation of youth and immigrants to areas where job opportunities exist.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
+            </div>
+
+        </div>
+
+        <!--row 4 -->
+        <div class="all-party-policy pipes small-12 medium-12 large-12 columns">
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>NDP</h4>
+                <p>Against Enbridge Northern Gateway and Keystone XL, but supportive of a better environmental review process and future pipeline plans.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Green</h4>
+                <p>The complete opposition of all pipeline proposals.
+                </p>
+                 <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
+            </div>
+
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Liberal</h4>
+                <p>Against Enbridge Northern Gateway, but supportive of TransCanada Keystone XL and Kinder Morgan Expansion.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Conservative</h4>
+                <p>The support and approval for Enbridge Northern Gateway, Keystone XL, Kinder Morgan Expansion, and Energy East.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
+            </div>
+
+        </div>
+        <!--row 5 -->
+        <div class="all-party-policy health small-12 medium-12 large-12 columns">
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>NDP</h4>
+                <p>The NDP promise 1.8 billion over 4 years to ensure universal access to prescription medication for Canadians. They would also spend 40 million over four years to develop a nationwide alzheimer's and dementia strategy. Lastly, the NDP would earmark 500 million to expand health care clinics and hire new health care providers.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Green</h4>
+                <p>The Greens would introduce a national pharmacare plan, which the party says will save the government $11 billion by buying in bulk.
+                </p>
+                 <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
+            </div>
+
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Liberal</h4>
+                <p>The Liberals propose $3 billion in funding over four years to improve the national health care system. This would focus on boosting home care, lower the cost of prescription drugs and improve mental health care. They would also work with the provinces to negotiate better prices on prescription drugs.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Conservative</h4>
+                <p>Recently chose not to renew the Canada Health Accord which increased funding for healthcare, common wait times, home care, and prescription drugs. Announced $31.5 million in funding for a plan on dementia.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
+            </div>
+
+        </div>
+        <!--row 6 -->
+        <div class="all-party-policy infra small-12 medium-12 large-12 columns">
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>NDP</h4>
+                <p>The NDP promised $1.5 billion per year to infrastructure funding in cities, which he says will be covered by using a portion of the gas tax. He also promised an additional Better Transit Plan.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Green</h4>
+                <p>Increase the Gas Tax Transfer to municipalities to five cents/litre to be used in funding the above sustainable transportation initiatives such as public transit, cycling and pedestrian infrastructure, and rural roads
+                </p>
+                 <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
+            </div>
+
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Liberal</h4>
+                <p>Liberals promise to invest in infrastructure, spending by $60-billion over the next decade, on top of the currently planned spending of $65-billion. Some of this infrastructure will be “green,” or environmentally friendly.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Conservative</h4>
+                <p>The Conservatives’ main infrastructure program was the New Building Canada Plan. It promises $53 billion over 10 years to be spent on infrastructure. They have pledged $150 million for communities across Canada to finance repairs and improvements to existing small public infrastructure.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
+            </div>
+
+        </div>
+        <!--row 7 -->
+        <div class="all-party-policy senate small-12 medium-12 large-12 columns">
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>NDP</h4>
+                <p>Thomas Mulcair supports complete abolition of the Senate. He would need to open the Constitution and have all provinces on board to do so.The NDP promised $1.5 billion per year to infrastructure funding in cities, which he says will be covered by using a portion of the gas tax. He also promised an additional Better Transit Plan.</p>
+                <h3 class="voteBtn btn voteButton" data-vote="ndp1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Green</h4>
+                <p>The Green Party of Canada supports the election of Senators through a system that ensures proportional representation.
+                </p>
+                 <h3 class="voteBtn btn voteButton" data-vote="green1">Vote</h3>
+            </div>
+
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Liberal</h4>
+                <p>Justin Trudeau wants an end to partisan senators and instead advocates for the creation of a merit-based appointment process. Trudeau expelled 32 Liberal senators from his caucus, and has declared there is no longer any such thing as a Liberal senator. The Liberal plan for Senate reform would not require a Constitutional amendment.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="liberal1">Vote</h3>
+            </div>
+
+            <div class="parties-pol small-12 medium-12 large-3 columns">
+                <h4>Conservative</h4>
+                <p>Stephen Harper wants a senate that would require all senators to be democratically elected. Before the campaign began, he announced that he would stop appointing Senators in an effort to force the provinces to agree to a reform. However, the legality of this is currently under fire.
+                </p>
+                <h3 class="voteBtn btn voteButton" data-vote="cons1">Vote</h3>
+            </div>
+
         </div>
         <div class="row">
         <h1 id="result"></h1>
         <div class="pie-chart" id="container"></div>
         </div>
-    
-    <!--</div>-->
-    <!--<div class="small-12 medium-3 large-3 columns">-->
-        <!--</div>-->
+
 
 </div>
 
